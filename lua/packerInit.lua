f2045214528da522fc923ead177b8cf3d032181d
linuxmobile <bdiez19@gmail.com>
2022-09-12 19:06:49 -0300

    Feat ✨: Added cmp-buffer

--- lua/packerInit.lua
+++ lua/packerInit.lua
@@ -44,6 +44,7 @@ return packer.startup(function(use)
 	})
 	use("nvim-lua/plenary.nvim") -- Useful lua functions used ny lots of plugins
 	use 'onsails/lspkind-nvim' -- vscode-like pictograms
+	use 'hrsh7th/cmp-buffer' -- nvim-cmp source for buffer words
     -- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
