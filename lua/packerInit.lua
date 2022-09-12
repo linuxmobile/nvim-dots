19b4b2e6286a4b14e5c4c2c3036df71f6cf6aba4
linuxmobile <bdiez19@gmail.com>
2022-09-12 20:42:39 -0300

    Feat ✨: Added git & gitsigns

--- lua/packerInit.lua
+++ lua/packerInit.lua
@@ -72,6 +72,8 @@ return packer.startup(function(use)
 		run = function() vim.fn["mkdp#util#install"]() end,
 	  })
 	use 'akinsho/nvim-bufferline.lua'
+	use 'lewis6991/gitsigns.nvim'
+	use 'dinhhuy258/git.nvim' -- For git blame & browse
     -- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
