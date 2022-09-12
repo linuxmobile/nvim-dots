de056572f9c4a1e53c8f8e1733e98366347c9b4a
linuxmobile <bdiez19@gmail.com>
2022-09-12 19:08:40 -0300

    Feat ✨: Added null-ls

--- lua/packerInit.lua
+++ lua/packerInit.lua
@@ -48,6 +48,8 @@ return packer.startup(function(use)
 	use 'hrsh7th/cmp-nvim-lsp' -- nvim-cmp source for neovim's built-in LSP
 	use 'hrsh7th/nvim-cmp' -- Completion
 	use 'neovim/nvim-lspconfig' -- LSP
+	use 'jose-elias-alvarez/null-ls.nvim' -- Use Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua
+	
     -- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
